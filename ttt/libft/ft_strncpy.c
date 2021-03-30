10a3ab539a32ca96cde2ad7ce4a53f99aad93eff
user42 <Vous@exemple.com>
2021-03-30 18:35:30 +0200

    OUI

---
+++ ttt/libft/ft_strncpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: araout   <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 07:09:22 by araout            #+#    #+#             */
+/*   Updated: 2018/11/06 07:09:23 by araout           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncpy(char *destination, const char *source, size_t num)
+{
+	unsigned int i;
+
+	if (!destination || !source)
+		return (destination);
+	i = 0;
+	while (source[i] != '\0' && i < num)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	while (i < num)
+	{
+		destination[i] = '\0';
+		i++;
+	}
+	return (destination);
+}
